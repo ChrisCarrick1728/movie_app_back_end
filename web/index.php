f919afd4f77b1a8531ada3b41f622995492adc79
Chris Carrick-2 <car17059@byui.edu>
2019-02-09 15:02:57 -0700

    initial commit

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=0">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  </body>
+</html>
