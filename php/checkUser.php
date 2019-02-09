978edbea6a8417c60652435cea3c96eea2f8e652
Chris Carrick-2 <car17059@byui.edu>
2019-02-09 16:37:31 -0700

    test

--- php/checkUser.php
+++ php/checkUser.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header ("Access-Control-Allow-Methods: GET,POST,OPTIONS,DELETE,PUT");
+//header("Access-Control-Allow-Origin: *");
+//header ("Access-Control-Allow-Methods: GET,POST,OPTIONS,DELETE,PUT");
 echo "'hello world! it's me :)'";
 ?>
