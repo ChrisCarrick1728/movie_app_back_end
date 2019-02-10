d711c034f2073bd82317c8b6debf03b20d6e1fb8
Chris Carrick-2 <car17059@byui.edu>
2019-02-09 19:58:08 -0700

    database functionality

--- web/php/checkUser.php
+++ web/php/checkUser.php
@@ -1,5 +1,33 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-echo($_SERVER['HTTP_ORIGIN']);
-echo true;
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET,POST");
+session_start();
+
+include ('db_connect.php');
+
+if (isset($_POST['userName']) && isset($_POST['password'])) {
+  try {
+    foreach ($db->query('SELECT id, salt FROM users WHERE username=\'' . $_POST['userName'] . '\'') as $userRow)
+    {
+      foreach ($db->query('SELECT hash FROM credentials WHERE user_id=' . $userRow['id']) as $credentialRow) {
+        // todo: compare hash's
+        $_SESSION['userAuthorized'] = 'true';
+        echo true;
+        return;
+      }
+    }
+
+  } catch (Exception $e) {
+    $_SESSION['userAuthorized'] = 'false';
+    echo false;
+    return;
+  }
+  echo false;
+  return;
+} else {
+  $_SESSION['userAuthorized'] = 'false';
+  echo false;
+  return;
+}
 ?>
---
+++ web/php/showAllMovie_genre.php
@@ -0,0 +1,18 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: GET,POST");
+session_start();
+
+include ('db_connect.php');
+
+try {
+  $statement = $db->query('SELECT * FROM movie_genre');
+  $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+  echo json_encode($results);
+} catch (Exception $e) {
+  $_SESSION['userAuthorized'] = 'false';
+  echo false;
+  return;
+}
+?>
